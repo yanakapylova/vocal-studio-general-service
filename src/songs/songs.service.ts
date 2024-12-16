2cbd7e792cdb507e214ce184117a9bc9f9c4731a
yanakapylova <yana.kapylova@innowise.com>
2024-12-16 17:06:57 +0100

    logs were added to each service (#5)

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,53 +1,66 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { CreateSongDto } from './dto/create-song.dto';
-import { UpdateSongDto } from './dto/update-song.dto';
-import { Cache } from 'cache-manager';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Logger } from '@nestjs/common';
-import { PrismaService } from 'prisma/prisma.service';
+import { Inject, Injectable } from "@nestjs/common";
+import { CreateSongDto } from "./dto/create-song.dto";
+import { UpdateSongDto } from "./dto/update-song.dto";
+import { Cache } from "cache-manager";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Logger } from "@nestjs/common";
+import { PrismaService } from "prisma/prisma.service";
+import { handleError } from "errorHandler";
 
 @Injectable()
 export class SongsService {
   constructor(
     private prisma: PrismaService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) {}
 
   async create(createSongDto: CreateSongDto) {
+    Logger.log("Creating a song");
     const { groups, ...newSong } = createSongDto;
-    await this.cacheManager.del('allGroups');
-    Logger.log('allGroups cache has been removed');
-    return await this.prisma.group.create({
-      data: {
-        ...newSong,
-        ...(groups && {
-          connect: groups.map((groupId) => ({ id: groupId })),
-        }),
-      },
-    });
+
+    await this.cacheManager.del("allSongs");
+    Logger.log("allSongs cache has been removed");
+
+    try {
+      return await this.prisma.group.create({
+        data: {
+          ...newSong,
+          ...(groups && {
+            connect: groups.map((groupId) => ({ id: groupId })),
+          }),
+        },
+      });
+    } catch (error) {
+      handleError("Error creating a song", error);
+    }
   }
 
   async findAll() {
-    Logger.log('GET all songs');
-    const value = await this.cacheManager.get('allSongs');
+    try {
+      Logger.log("Getting all songs");
+      const value = await this.cacheManager.get("allSongs");
 
-    if (value) {
-      Logger.log('"allSongs" has been taken from cache');
-      return value;
-    } else {
-      const result = await this.prisma.song.findMany({
-        include: {
-          groups: true,
-        },
-      });
-      await this.cacheManager.set('allSongs', result);
-      Logger.log("'allSongs' has been cached");
-      return result;
+      if (value) {
+        Logger.log('"allSongs" has been taken from cache');
+        return value;
+      } else {
+        const result = await this.prisma.song.findMany({
+          include: {
+            groups: true,
+          },
+        });
+        await this.cacheManager.set("allSongs", result);
+        Logger.log("'allSongs' has been cached");
+        return result;
+      }
+    } catch (error) {
+      handleError("Error retrieving songs", error);
     }
   }
 
   async findOne(id: number) {
     try {
+      Logger.log("Getting the song with id " + id);
       const result = await this.prisma.song.findUniqueOrThrow({
         where: { id },
         include: {
@@ -55,13 +68,14 @@ export class SongsService {
         },
       });
       return result;
-    } catch {
-      `Песня с ID ${id} не найдена`;
+    } catch (error) {
+      handleError(`Error retrieving the song with id ${id}`, error);
     }
   }
 
   async update(id: number, updateSongDto: UpdateSongDto) {
     try {
+      Logger.log("Updating the song with id " + id);
       const existingSong = await this.prisma.song.findUniqueOrThrow({
         where: { id },
         include: {
@@ -78,19 +92,20 @@ export class SongsService {
         }),
       };
 
-      await this.cacheManager.del('allSongs');
-      Logger.log('allSongs cache has been removed');
+      await this.cacheManager.del("allSongs");
+      Logger.log("allSongs cache has been removed");
       return await this.prisma.song.update({
         where: { id },
         data: updateData,
       });
-    } catch {
-      `Песня с ID ${id} не найдена`;
+    } catch (error) {
+      handleError(`Error updating the song with id ${id}`, error);
     }
   }
 
   async remove(id: number) {
     try {
+      Logger.log("Deleting the song with id " + id);
       await this.prisma.song.findUniqueOrThrow({
         where: { id },
       });
@@ -99,10 +114,10 @@ export class SongsService {
         where: { id },
       });
 
-      await this.cacheManager.del('allSongs');
-      Logger.log('allSongs cache has been removed');
-    } catch {
-      console.log(`Song with ID ${id} not found`);
+      await this.cacheManager.del("allSongs");
+      Logger.log("allSongs cache has been removed");
+    } catch (error) {
+      Logger.error(`Error deleting the song with id ${id}` + error);
     }
   }
 }
